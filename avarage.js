d8d022fad892a0a7d064bd902fba3d1b6316c5b8
shojol021 <shojol021@gmail.com>
2023-01-26 18:02:18 +0600

    finding avarage

---
+++ avarage.js
@@ -0,0 +1,11 @@
+var math = 97;
+var bangla = 76;
+var english = 78;
+var physics = 68;
+var biology = 38;
+
+var total = math + bangla + english + physics + biology;
+var avg = total /5;
+
+
+console.log(avg)
