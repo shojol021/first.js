afe477d664c90fabb9f27eadd6f39896e224317e
shojol021 <shojol021@gmail.com>
2023-02-05 16:55:16 +0600

    search string

--- string.js
+++ string.js
@@ -3,4 +3,10 @@ var mName = 'Hossain';
 var lName = 'Shojol';
 var fullName = fName + ' ' + mName + ' ' + lName;
 
-console.log(fullName)
+console.log(fullName);
+
+const a = 'ami banglay gaan Gai, ami banglar gaan Gai, ami amr ami K chirodin...';
+const s = 'Gai';
+// const aLo = a.toLowerCase();
+// const sLo = s.toLowerCase();
+console.log(a.toLowerCase().includes(s.toLowerCase()));
